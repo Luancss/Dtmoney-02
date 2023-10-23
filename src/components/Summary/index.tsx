152bfa376970d474b47913318c7409395c3031ce
Luancss <ventobo123@gmail.com>
2023-10-23 15:52:34 -0300

    Component - Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,30 @@
+import { SummaryCard, SummaryContainer } from "./styles";
+import {ArrowCircleDown, ArrowCircleUp, CurrencyDollar} from 'phosphor-react'
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e"/>
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f76a68"/>
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Entradas</span>
+          <CurrencyDollar size={32} color="#fff"/>
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
