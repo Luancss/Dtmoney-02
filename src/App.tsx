95498458e2b3291aaccdd8ea52ffebeb2512123a
Luancss <ventobo123@gmail.com>
2023-10-23 15:28:43 -0300

    Component - Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import {defaultTheme} from './styles/themes/default'
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
-    <h1>Hello World</h1>
+      <Transactions/>
+
     </ThemeProvider>
   )
 }
