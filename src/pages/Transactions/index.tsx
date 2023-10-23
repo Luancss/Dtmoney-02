da7c31c8f2b01305e13b4d141b00868ca3d36d4f
Luancss <ventobo123@gmail.com>
2023-10-23 16:53:42 -0300

    Component - SeachForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionTable, TransactionsContainer } from "./styles";
 
 export function Transactions() {
@@ -9,6 +10,7 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+      <SearchForm/>
         <TransactionTable>
           <tbody>
             <tr>
