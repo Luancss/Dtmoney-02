b0d354fe928732b6c8a28841b91919be7eb34f6d
Luancss <ventobo123@gmail.com>
2023-11-06 17:40:59 -0300

    Calculating transaction summary

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,5 @@
 import { styled } from "styled-components";
 
-
 export const HeaderContainer = styled.header`
   background: ${props => props.theme['gray-900']};
   padding: 2.5rem 0 7.5rem;
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,24 @@ import { TransactionsContext } from "../../contexts/TransactionsContext";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if(transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
   console.log(transactions);
 
   return (
@@ -15,21 +33,21 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f76a68" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
-          <span>Entradas</span>
+          <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
