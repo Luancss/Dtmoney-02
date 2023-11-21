0828d733b3d6ead9ec9309b512030a9d05fb219c
Luancss <ventobo123@gmail.com>
2023-11-21 16:11:28 -0300

    Applying the memo

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -5,6 +5,7 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
+import {memo} from 'react'
 
 
 const searchFormSchema = z.object({
@@ -13,7 +14,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInput = z.infer<typeof searchFormSchema>;
 
-export function SearchForm() {
+ function SearchFormComponent() {
   const fetchTransactions = useContextSelector(TransactionsContext, (context) => {
     return context.fetchTransactions
   })
@@ -45,3 +46,5 @@ export function SearchForm() {
     </SearchFormContainer>
   );
 }
+
+export const SearchForm = memo(SearchFormComponent);
